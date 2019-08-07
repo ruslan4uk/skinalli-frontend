16e4f3d5c76f0ab0aef8242d43dfe5d9ff33c609
Ruslan <rusel91@idz.ru>
2019-08-07 16:59:26 +0400

    fix

--- pages/admin/category/index.vue
+++ pages/admin/category/index.vue
@@ -4,7 +4,7 @@
             <v-flex xs12>
                 <!-- <h2 class="mb-5">Категории</h2> -->
 
-                <v-data-table :headers="headers" :items="desserts" sort-by="calories" class="elevation-1 mb-5">
+                <v-data-table :headers="headers" :items="category" sort-by="calories" class="elevation-1 mb-5">
                     <template v-slot:top>
                         <v-toolbar flat color="white">
                             <v-toolbar-title>Категории</v-toolbar-title>
@@ -34,12 +34,15 @@
 
                                     <v-card-actions>
                                         <v-spacer></v-spacer>
-                                        <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                                        <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                                        <v-btn color="blue darken-1" text @click="close">Отмена</v-btn>
+                                        <v-btn color="blue darken-1" text @click="save">Сохранить</v-btn>
                                     </v-card-actions>
                                 </v-card>
                             </v-dialog>
                         </v-toolbar>
+                    </template>
+                    <template v-slot:item.active="{ item }">
+                        {{ item.active ? 'Активно' : 'Не активно' }}
                     </template>
                     <template v-slot:item.action="{ item }">
                         <v-icon small class="mr-2" @click="editItem(item)" >edit</v-icon>
@@ -61,13 +64,14 @@ export default {
     data: () => ({
       dialog: false,
       headers: [
-        { text: 'Название', align: 'left', value: 'name', },
-        { text: 'Активно', align: 'right', value: 'active', sortable: false },
+        { text: 'Название', align: 'left', value: 'name' },
+        { text: 'Активно', align: 'right', value: 'active' },
         { text: 'Действие', align: 'right', value: 'action', sortable: false },
       ],
-      desserts: [],
+      category: [],
       editedIndex: -1,
       editedItem: {
+        id: '',
         name: '',
         active: 0,
       },
@@ -90,122 +94,50 @@ export default {
     },
 
     created () {
-      this.initialize()
+        this.initialize ()
     },
 
     methods: {
-      initialize () {
-        this.desserts = [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            active: 1,
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            active: 1,
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            active: 1,
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            active: 1,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            active: 1,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            active: 1,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            active: 1,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            active: 1,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            active: 1,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            active: 1,
-          },
-        ]
-      },
+        initialize () {
+            this.$axios.get(this.$route.path).then(res => {
+                this.category = res.data.data 
+            })
+        },
 
-      editItem (item) {
-        this.editedIndex = this.desserts.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialog = true
-      },
+        editItem (item) {
+            this.editedIndex = this.category.indexOf(item)
+            this.editedItem = Object.assign({}, item)
+            this.dialog = true
+        },
 
-      deleteItem (item) {
-        const index = this.desserts.indexOf(item)
-        confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
-      },
+        deleteItem (item) {
+            const index = this.category.indexOf(item)
+            confirm('Are you sure you want to delete this item?') && this.category.splice(index, 1)
+            this.$axios.delete(this.$route.path + '/' + item.id).then(res => {
+                console.log(res);                
+            })
+        },
 
-      close () {
-        this.dialog = false
-        setTimeout(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        }, 300)
-      },
+        close () {
+            this.dialog = false
+            setTimeout(() => {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            }, 300)
+        },
 
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.desserts[this.editedIndex], this.editedItem)
-        } else {
-          this.desserts.push(this.editedItem)
-        }
-        this.close()
-      },
+        save () {
+            this.$axios.post(this.$route.path, this.editedItem).then(res => {
+                console.log(res.data);
+                if (this.editedIndex > -1) {
+                    Object.assign(this.category[this.editedIndex], res.data.data)
+                } else {
+                    this.category.push(res.data.data)
+                }
+                this.close()
+            });
+            
+        },
     },
 }
 </script>
